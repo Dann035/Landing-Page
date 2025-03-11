0881968fae00abab7940975442d2d13bb16a8134
Dann035 <landadlh603@gmail.com>
2025-03-11 03:28:54 +0100

    add component Navbar

---
+++ src/js/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav className="nav navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid d-flex justify-content-between">
+        <div className="ms-5">
+          <a className="navbar-brand" href="#">
+            Start Bootstrap
+          </a>
+        </div>
+        <div className="me-5">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon black"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
