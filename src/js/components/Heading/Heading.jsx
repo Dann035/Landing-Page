f749d18f2fb2c4e231d082f9264bf7cd5518338c
Dann035 <landadlh603@gmail.com>
2025-03-11 03:29:47 +0100

    add heading component

---
+++ src/js/components/Heading/Heading.jsx
@@ -0,0 +1,21 @@
+import "./Heading.css";
+const Heading = () => {
+  return (
+    <div className="mx-5 my-3">
+      <div className="alert bg-green-light p-5">
+        <h1>A Warm Welcome</h1>
+        <p>
+          Nota: las tarjetas tienen contenido diferente, piensa en DRY (Don't
+          Repeat Yourself), que significa: "No Te Repitas" y declara solo un
+          componente , luego usa props para manejar contenido diferente. Nota:
+          las tarjetas tienen contenido diferente, piensa en DRY (Don't Repeat
+          Yourself), que significa: "No Te Repitas" y declara solo un componente
+          , luego usa props para manejar contenido diferente.
+        </p>
+        <button className="btn btn-primary">Call to action !</button>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
